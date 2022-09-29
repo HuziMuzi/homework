ff9716d611b6e183aa52422d960e2c0442807671
HuziMuzi <laif17860@mail.ru>
2022-09-29 16:46:49 +0300

    test commit

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function HW10() {
-    // useSelector, useDispatch
+
     const loading = false
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+
         console.log('loading...')
     };
 
